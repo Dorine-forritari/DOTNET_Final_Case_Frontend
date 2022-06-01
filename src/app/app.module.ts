a328dec44f83e9f212521ef1c08e651d58e26842
Dorine van Belzen <dorinevanbelzen@gmail.com>
2022-06-01 15:56:46 +0200

    feat: add catalogue component and mock data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPage } from './pages/main/main.page';
+import { CatalogueComponent } from './components/catalogue/catalogue.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPage
+    MainPage,
+    CatalogueComponent
   ],
   imports: [
     BrowserModule,
