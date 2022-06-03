2c1ace3c762aeb037707b051607fc1944e5b2dec
Dorine van Belzen <dorinevanbelzen@gmail.com>
2022-06-03 11:44:30 +0200

    fix: move search functionality to search page and implement search bar in both main and catalogue page

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { mockProjects } from 'src/app/data/mock-data';
+import { Project } from 'src/app/models/project.model';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchPage implements OnInit {
 
-  constructor() { }
+  projects: Project[] = [];
+  
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // Filter projects based on search input in params
+    this.route.params.subscribe(params => {
+      if (params['searchInput']) {
+        this.projects = mockProjects.filter(project => project.title.toLowerCase().includes(params['searchInput'].toLowerCase()));
+      } else {
+        this.projects = mockProjects;
+      }
+    })
   }
 
 }
