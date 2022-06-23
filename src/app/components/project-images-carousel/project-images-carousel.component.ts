504ebecdd2edfb90c77ba49b1eba0abb42ae2e22
Leroy96 <leroyb96@gmail.com>
2022-06-23 10:55:48 +0200

    CHORE: added project photos

--- src/app/components/project-images-carousel/project-images-carousel.component.ts
+++ src/app/components/project-images-carousel/project-images-carousel.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-project-images-carousel',
   templateUrl: './project-images-carousel.component.html',
-  styleUrls: ['./project-images-carousel.component.scss']
+  styleUrls: ['./project-images-carousel.component.scss'],
 })
 export class ProjectImagesCarouselComponent implements OnInit {
+  @Input() photo: string | undefined;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
